9e1d51dba13c9efe01cda5ffba194bf3426e5efd
amelmnd <amelmnd.dev@gmail.com>
2024-07-16 07:47:31 +0200

    new step adapte page skills and project

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -7,50 +7,106 @@ export default function Projects() {
         <h2>Mes Projets</h2>
         <p> </p>
         <div class='colonne'>
+          {/* TODO BOUCLE SUR LES DONNEE RECUPERE SOIT DB SOIT FILE */}
           <article id='webagency'>
-            <h3>
-              <a href='http://amel-mennad.fr/webagency/'> WebAgency</a>
-            </h3>
-            <a href='http://amel-mennad.fr/webagency/'>
-              {' '}
+            <div class='card dark html css'>
               <img
-                src='portfolio_elements/images/img_projets/projets-pro/webagency.png'
-                class='proimg'
-                alt='projet webagency'
-              />{' '}
-            </a>
-            {/* <p><span class="fab fa-html5"><span class="fab fa-css3-alt"></span></p>  */}
-          </article>
-          <article id='travelagency'>
-            <h3>
-              <a href='http://amel-mennad.fr/travelagency/'> Travel Agency</a>
-            </h3>
-            <a href='http://amel-mennad.fr/travelagency/'>
-              {' '}
-              <img
-                src='portfolio_elements/images/img_projets/projets-pro/travelagency.png'
-                class='proimg'
-                alt='projet travelagency'
-              />{' '}
-            </a>
-            {/* <p><span class="fab fa-html5"><span class="fab fa-css3-alt"></span></p>  */}
-          </article>
-          <article id='otstrasbourg'>
-            <h3>
-              <a href='https://projetwp-strasbourg.amel-mennad.fr/'>
-                {' '}
-                Travel Agency
-              </a>
-            </h3>
-            <a href='https://projetwp-strasbourg.amel-mennad.fr/'>
-              {' '}
-              <img
-                src='portfolio_elements/images/img_projets/projets-pro/strasbourg.png'
-                class='proimg'
-                alt='projet office du tourisme strasbourg'
-              />{' '}
-            </a>
-            {/* <p><span class="fab fa-wordpress-simple"></span></p>  */}
+                class='card-img-top'
+                // TODO URL ONLINE PICTURE PROJECT
+                src='img/projets/siteweb/webagency.png'
+                alt=''
+              />
+              <div class='card-body'>
+                <h5 class='card-title'>Web Agency</h5>
+                <button
+                  type='button'
+                  class='btn btn-info'
+                  data-toggle='modal'
+                  data-target='#webAgency'
+                >
+                  Détail
+                </button>
+                <button type='button' class='btn btn-info'>
+                  <a
+                    href='https://webagency-ocr.amel-mennad.fr/'
+                    target='_blank'
+                  >
+                    Site
+                  </a>
+                </button>
+                {/* <!-- Modal --> DATA DE LA MODAL : */}
+                <div
+                  class='modal fade'
+                  id='webAgency'
+                  tabindex='-1'
+                  role='dialog'
+                  aria-labelledby='webAgencyTitle'
+                  aria-hidden='true'
+                >
+                  <div class='modal-dialog' role='document'>
+                    <div class='modal-content'>
+                      <div class='modal-header'>
+                        <h5 class='modal-title' id='webAgencyTitle'>
+                          Web Agency
+                        </h5>
+                      </div>
+                      <div class='modal-body'>
+                        <h3>Projet Opendeclic, Openclassrooms</h3>
+                        <p>Consignes</p>
+                        <p>
+                          Vous venez d'être embauché(e) par une agence Web, la
+                          WebAgency, qui réalise des sites web pour différents
+                          clients. Cependant, ils reconnaissent eux-mêmes que
+                          leur site actuel est vieillissant car ils ont peu de
+                          temps pour travailler dessus. Certes, il paraît que
+                          les cordonniers sont les plus mal chaussés, mais tout
+                          de même... On vous propose donc, pour votre première
+                          mission, de vous occuper de la refonte du site de
+                          l'agence. Le site doit tenir sur une page avec un menu
+                          qui reste visible, en haut de la page, meme lorsque
+                          l'on se déplace dans la page.
+                        </p>
+                        <p>
+                          Les différentes sections attendues sont :Un premier
+                          écran d'accueil de bienvenue, très visuel
+                          <ul>
+                            <li>La liste des services offerts par l'agence</li>
+                            <li>Des exemples de projets déjà réalisés</li>
+                            <li>
+                              Une carte avec un formulaire de contact (on ne
+                              vous demande pas de faire marcher le formulaire,
+                              juste de l'afficher).
+                            </li>
+                            <li>
+                              Le site sera réalisé en HTML et CSS (sans
+                              framework tel que Bootstrap)
+                            </li>
+                          </ul>
+                        </p>
+                        <p>Langages : HTML, CSS</p>
+                      </div>
+                      <div class='modal-footer'>
+                        <button
+                          type='button'
+                          class='btn btn-secondary'
+                          data-dismiss='modal'
+                        >
+                          Close
+                        </button>
+                        <button type='button' class='btn btn-info'>
+                          <a
+                            href='https://webagency-ocr.amel-mennad.fr/'
+                            target='_blank'
+                          >
+                            Site
+                          </a>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </article>
         </div>
       </section>
