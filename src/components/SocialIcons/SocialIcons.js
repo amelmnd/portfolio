a6470186a8c8146302c094e4af9bb9c66b172ddb
amelmnd <amelmnd.dev@gmail.com>
2025-07-17 20:16:49 +0200

    portfolio adapt social medal visibility

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -4,75 +4,88 @@ import styles from './SocialIcons.module.css'
 import { Icon } from '@iconify/react'
 import CodingameIcon from '../../assets/codingameIcon.png';
 import Image from 'next/image';
+import { color } from 'framer-motion';
 
 export default function SocialIcons({ width = 32, height = 32 }) {
 	const socialLinks = [
-
-		{
-			title: 'Email',
-			icon: 'wpf:message',
-			href: 'mailto:amelmnd.dev@gmail.com',
-			isCustom: false,
-
-		},
-		{
-			title: 'LinkedIn',
-			icon: 'simple-icons:linkedin',
-			href: 'https://www.linkedin.com/in/amel-mennad/',
-			isCustom: false,
-
-		},
-		{
-			title: 'GitHub',
-			icon: 'simple-icons:github',
-			href: 'https://github.com/amelmnd',
-		},
-		{
-			title: 'CodePen',
-			icon: 'simple-icons:codepen',
-			href: 'https://codepen.io/amel_dev',
-			isCustom: false,
-
-		},
-		{
-			title: 'Freecodecamp',
-			icon: 'cib:freecodecamp',
-			href: 'https://www.freecodecamp.org/amel_dev',
-			isCustom: false,
-
-		},
-		{
-			title: 'Codingame',
-			icon: CodingameIcon,
-			href: 'https://www.codingame.com/profile/3eb108f7afc5d60d15c961e2bef3ed4c7207735',
-			isCustom: true,
-		},
-		{
-			title: 'Hackerrank',
-			icon: 'cib:hackerrank',
-			href: 'https://www.hackerrank.com/profile/amel_dev',
-			isCustom: false,
-
-		},
-	]
+    {
+      title: 'Email',
+      icon: 'wpf:message',
+      href: 'mailto:amelmnd.dev@gmail.com',
+      isCustom: false,
+      active: true,
+    },
+    {
+      title: 'LinkedIn',
+      icon: 'simple-icons:linkedin',
+      href: 'https://www.linkedin.com/in/amel-mennad/',
+      isCustom: false,
+      active: true,
+    },
+    {
+      title: 'GitHub',
+      icon: 'simple-icons:github',
+      href: 'https://github.com/amelmnd',
+      active: true,
+    },
+    {
+      title: 'CodePen',
+      icon: 'simple-icons:codepen',
+      href: 'https://codepen.io/amel_dev',
+      isCustom: false,
+      active: false,
+    },
+    {
+      title: 'Freecodecamp',
+      icon: 'cib:freecodecamp',
+      href: 'https://www.freecodecamp.org/amel_dev',
+      isCustom: false,
+      active: false,
+    },
+    {
+      title: 'Codingame',
+      icon: CodingameIcon,
+      href: 'https://www.codingame.com/profile/3eb108f7afc5d60d15c961e2bef3ed4c7207735',
+      isCustom: true,
+      active: false,
+    },
+    {
+      title: 'Hackerrank',
+      icon: 'cib:hackerrank',
+      href: 'https://www.hackerrank.com/profile/amel_dev',
+      isCustom: false,
+      active: false,
+    },
+  ];
 	return (
-		<div className={styles.iconsContainer}>
-			{socialLinks.map(({ title, icon, href, isCustom  }) => (
-				<Link
-					key={title}
-					href={href}
-					target="_blank"
-					rel="nofollow noreferrer noopener"
-					className={`${styles.iconLink} ${styles[title]}`}
-
-				>
-					{isCustom ? (
-						<Image src={CodingameIcon} alt="Codingame" width={32} height={32} />
-					) : (
-						<Icon icon={icon} width={width} height={height} className={styles.skillIcon} />
-					)}
-				</Link>
-			))}
-		</div>
-	)
+    <div className={styles.iconsContainer}>
+      {socialLinks
+        .filter((item) => item.active)
+        .map(({ title, icon, href, isCustom }) => (
+          <Link
+            key={title}
+            href={href}
+            target='_blank'
+            rel='nofollow noreferrer noopener'
+            className={`${styles.iconLink} ${styles[title]}`}
+          >
+            {isCustom ? (
+              <Image
+                src={CodingameIcon}
+                alt='Codingame'
+                width={32}
+                height={32}
+              />
+            ) : (
+              <Icon
+                icon={icon}
+                width={width}
+                height={height}
+                className={styles.skillIcon}
+              />
+            )}
+          </Link>
+        ))}
+    </div>
+  );
 }
